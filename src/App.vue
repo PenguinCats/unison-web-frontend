b9033466e9105f492ec762becca86031af84650a
Binjie Zhang <bj_zhang@seu.edu.cn>
2021-09-08 12:00:14 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <n-layout>
+      <n-layout-header>
+        <n-grid x-gap="12" :cols="4">
+          <n-gi span="1" style="width: 100%">
+            Logo
+          </n-gi>
+          <n-gi span="2" style="width: 100%">
+            <MenuComponent></MenuComponent>
+          </n-gi>
+          <n-gi span="1">
+            UserComponent
+          </n-gi>
+        </n-grid>
+      </n-layout-header>
+      <n-layout-content content-style="padding: 24px;">平山道</n-layout-content>
+      <n-layout-footer>成府路</n-layout-footer>
+    </n-layout>
   </div>
-  <router-view/>
+<!--  <n-layout>-->
+<!--    <n-layout-header>-->
+<!--      <n-grid x-gap="12" :cols="4">-->
+<!--        <n-gi span="3">-->
+<!--          Nav-->
+<!--        </n-gi>-->
+<!--        <n-gi span="1">-->
+<!--          UserComponent-->
+<!--        </n-gi>-->
+<!--      </n-grid>-->
+<!--    </n-layout-header>-->
+<!--    <n-layout-content content-style="padding: 24px;">平山道</n-layout-content>-->
+<!--    <n-layout-footer>成府路</n-layout-footer>-->
+<!--  </n-layout>-->
+<!--  <div id="nav">-->
+<!--    <router-link to="/">Home</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--  </div>-->
+<!--  <router-view/>-->
 </template>
+
+<script>
+import MenuComponent from '@/components/Menu/MenuComponent.vue';
+
+export default {
+  components: { MenuComponent },
+};
+
+</script>
 
 <style lang="less">
 #app {
@@ -15,16 +57,17 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.n-layout-header,
+.n-layout-footer {
+  background: rgba(128, 128, 128, 0.2);
+  padding: 24px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.n-layout-sider {
+  background: rgba(128, 128, 128, 0.3);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.n-layout-content {
+  background: rgba(128, 128, 128, 0.4);
 }
 </style>
---
+++ src/components/Menu/MenuComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" />
+</template>
+
+<script>
+import { h, ref } from 'vue';
+
+import {
+  CatchingPokemonFilled,
+  AllInboxFilled,
+  HelpOutlineRound,
+} from '@vicons/material';
+
+import {
+  VirtualMachine,
+} from '@vicons/carbon';
+
+import { NIcon } from 'naive-ui';
+
+function renderIcon(icon) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+const menuOptions = [
+  {
+    label: () => h(
+      'a',
+      {
+        href: 'https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F',
+        target: '_blank',
+        rel: 'noopenner noreferrer',
+      },
+      '主页',
+    ),
+    key: 'index',
+    icon: renderIcon(CatchingPokemonFilled),
+  },
+  {
+    label: '计算实例',
+    key: 'instance',
+    icon: renderIcon(VirtualMachine),
+  },
+  {
+    label: '站内消息',
+    key: 'inbox',
+    icon: renderIcon(AllInboxFilled),
+  },
+  {
+    label: '帮助手册',
+    key: 'help',
+    icon: renderIcon(HelpOutlineRound),
+  },
+];
+
+export default {
+  name: 'MenuComponent',
+  data() {
+    return {
+      activeKey: ref(null),
+      menuOptions,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
